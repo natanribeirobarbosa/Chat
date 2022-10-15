6f4d8e4ac2fefd7b5e203ba9c4a998548b059bb8
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2022-10-15 01:18:55 -0300

    Limpando tudo para começar de novo.

--- app/chat.js
+++ app/chat.js
@@ -53,205 +53,37 @@ app.use(expressSession({
 		
 }))
  
- 
-
- 
 
 var id = 0 
 var mensagens = {}
 
- //Esse middleware precisa ser executado sempre e apenas quando um úsuario entrar na página principal. 
- 
- 
 app.get('/', (req, res) => { 
-	//req.session.v = '1'
-	res.render('index') 
-	console.log('/, número de usuarios online: '+id) 
-	console.log('/,'+JSON.stringify(mensagens)) 
-	
-	
-})
 
-app.post('/verifica', (req, res) => {
-	if(req.session.log != undefined) {
-		res.send('já tem uma guia aberta??'+req.session.name)
-		
-	}
-	res.end()
+	
+	
 })
 
 app.get('/chat', (req, res) => {
-	//console.log('req.session.id: '+req.session.id)
-	 
-	
-	if(req.session.name == null){
-		id++ 
-		
-		req.session.name = id  
-		req.session.log = true 
-		//req.session.mess = 0 
-		console.log('/chat, o user nº: '+id+'ficou com o name: '+req.session.name)
-		 
-		//req.session.envios = {}  
-		
-		mensagens[req.session.name] = []  
-		//console.log(mensagens)
-		  
-		 var número =  id % 2 
-		if(número != 0){
-			if(mensagens[id+1] == undefined){
-			req.session.dest = id+1
-			console.log('/chat, o user: '+id+' ficou com o dest: '+req.session.dest)
-			res.render('chat', {mensagem: 1})
-			req.session.log = req.session.name}else{
-				
-			}
-			
-		}else{ 
-			req.session.dest = id-1
-			console.log('/chat, o user: '+id+' ficou com o dest: '+req.session.dest)
-			res.render('chat', {mensagem: 1}) 
-			//req.session.log = req.session.name
-			
-			/*var contagemRegressiva = setTimeout(() => {
-			mensagens[req.session.name] = undefined
-		}, 10000)*/
-			
-		} 
-	}else{  
-		//console.log(mensagens)
-		//if(mensagens[req.session.name] != undefined){
-		//res.send('já tem uma guia aberta??'+req.session.name) 
-			
-		//}else{
-		res.render('chat', {id: req.session.name})
-		}
-		
-			/*var contagemRegressiva = setTimeout(() => {
-			mensagens[req.session.name] = undefined
-		}, 10000)*/
-			
-	
-		//}
-	 
-	//   
 	
 })  
- 
 
 app.post('/api/send', (req, res) => {
 	
-	var dest = req.session.dest
-	var mess = req.body.texto
-	//console.log(dest,mess)
-	//req.session.mess.push(mess) 
-	//req.session.envios[dest].destinos[dest]= [[mess],['data']] 
-	mensagens[`${dest}`].push(mess)  
-	 //mensagens[name] 
-	 
-	//remetente[dest].push = [{mess: mess}]
-	//console.log(remetente)
-	//console.log(remetente)
-	//mensagens[dest].dest = req.session.mess    
-	//req.session.mess = 0 
-	//console.log('99 - objeto com todas as mensagens: '+JSON.stringify(mensagens))
-	res.send( {resp: '<b>'+mess+'</b>'})  
+
 		 
 })  
-var contagemRegressiva = undefined
-    /* function saidaForçada(name) { 
-		 	var contagemRegressiva = setTimeout(() => { 
-			mensagens[name] = undefined
-		}, 5000)
-		 
-	 }*/ 
-   
-  
-app.post('/api/resp', (req, res) => {
-	 if(req.session.name > id){ 
-		 id = req.session.name  
-	 } 
 
-		if(mensagens[req.session.dest] == undefined || mensagens[req.session.name] == undefined){  
-		console.log('/api/resp o user:'+req.session.name+' perdeu o seu parceiro') 
-		//req.session.log = null
+app.post('/api/resp', (req, res) => {
 	
-		
-		mensagens[req.session.name] = undefined
-		req.session.name = null   
-		req.session.dest = null  
-		id=id-1
-		res.send({mensagem: 1})
- 
-		res.end()
-	} else{    
-	
-	   
-	//console.log('/api/resp') 
-	//var mensagensRecebidas = req.session.mess 
-	var meuId = req.session.name
-	var vetor = mensagens[meuId] 
-	
-	//console.log('id do user: '+req.session.name) 
-	//console.log('mensagens do user: '+mensagens[meuId].length) 
-	if(mensagens[meuId].length != 0){
- 
-	
-		res.send({mensagem: vetor})
-		mensagens[meuId].splice(0, vetor.length)
-		//req.session.indice = 49
-		 
- 
-	 
-	
-  
-		}else{res.send({mensagem: null})}
-	 
-  }  
-  
-  
-			//saidaForçada(req.session.name)
-		 
 })   
 
 app.post('/api/bye', (req, res) => {
-	if(req.session.name == null){
-		res.end()
-	}else{
-	 
-	
-	//console.log('/api/resp, este é o middleware que apaga os dados logo depois...')
-		//if(req.query == ) 
-		//console.log('rota de sáida de usuario, req: '+JSON.stringify(req.query))
-		
-		//req.session.log = null
-		mensagens[req.session.name] = undefined
-		req.session.name = null
-		//console.log(mensagens) 
-		id-- 
-		//mensagens[req.session.name] = [] 
-		res.end()
-	console.log('/api/bye - alguém saiu, o número de pessoas online no momento: '+id)}
+
 	
 })
  
-  
-
-
- 
- 
- 
-
-
-
-
  app.post('/loading', (req, res) =>{
-	 //console.log(id)
-		if(mensagens[req.session.dest] != undefined){
-			res.send({res: true}) 
-		}else(
-			res.send({res: false}) 
-		)
+
  })
  
 // página 404 personalizada
