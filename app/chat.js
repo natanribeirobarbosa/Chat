3b22d2d602bfaa3aa6c95af4f915890ba54573ef
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2022-10-16 16:53:46 -0300

    Agora, um vetor é quem define os pares de conversa, enquanto uma variavel define os id's de cada úsuario.

--- app/chat.js
+++ app/chat.js
@@ -1,16 +1,7 @@
 /*
 
-Para consertar o erro da var "id" sendo diminuida sem sentido, é só redirecionar o user para outra rota?????????????????????
--não
-Para consertar o erro da var "id" sendo diminuida sem sentido, é só ao invés de fazer isso por um evento no front-end, um setTimeOut no back-end?????????????????
--não
-Para consertar o erro da var "id" sendo diminuida sem sentido, é só arrumar um jeito de não recarregar a página e manipular os elementos de acordo com as respostas vindas do servidor, assim a view loading seria extinta???????????????????
-
-
-
-
-Como funciona a aplicação?
-	1-O user ganha um identificador que serve 
+variavel id = serve para nomear os úsuarios com um "id".
+array onlineUsers = serve para definir com quem cada pessoa irá conversar.
 
 */
 
@@ -41,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
 //configurando o cookie-parser
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser') 
 app.use(cookieParser(credentials.cookieSecret))
 
 //configurando o express-session
@@ -50,43 +41,98 @@ app.use(expressSession({
 	resave: false, 
 	saveUninitialized: false,
 	secret: credentials.cookieSecret, 
-		
+	maxAge: 30 * 24 * 60 * 60 * 1000
 }))
  
 
-var id = 0 
+var id = -1
 var mensagens = {}
+var onlineUsers = []
 
 app.get('/', (req, res) => { 
+	res.render('index')
 
-	
 	
 })
-
-app.get('/chat', (req, res) => {
+  
+app.get('/chat', (req, res) => { 
 	
-})  
-
-app.post('/api/send', (req, res) => {
 	
-
+	if(req.session.name == null){//Se for a primeira vez do úsuario no site
+		id++ 
+		req.session.name = id
+		}
+		
+		if(onlineUsers.indexOf(req.session.name) == -1){//o úsuario está online? se não, agora está.
+			onlineUsers.push(req.session.name)
+			}
+		
+		
+		var user = {id: req.session.name,
+							position: onlineUsers.indexOf(req.session.name)
+							} 
 		 
+		if(req.session.dest == undefined){ //Já tem um destino definido? caso não arranjamos um novo!
+		 
+		if(user.position % 2 != 0){//A posição do úsuario no array é impar? se sim,  seu destino é uma posição atrás no array
+			var i = onlineUsers.indexOf(user.id)-1
+			console.log( onlineUsers.indexOf(user.id)-1)
+			user.dest = onlineUsers[i]
+			req.session.dest = user.dest
+		}else{//Então é ... Seu destino estará uma casa a frente...
+			var i = onlineUsers.indexOf(user.id)+1
+			if(onlineUsers[i] == undefined){//Já existe um destino? se não, terá de esperar
+				user.dest = -1
+			}else{
+				user.dest = onlineUsers[i]
+				req.session.dest = user.dest
+			} 
+		}
+		}
+			
+			if(onlineUsers.indexOf(user.dest) == -1){//o destino está online? se não, o user terá que esperar
+				res.render('chat', {message: 1})
+			}else{
+				res.render('chat', {message: 2}) 
+			}
+			 //console.log(req.session.dest)user
+	console.log('"/" usuario '+req.session.name+' acabou de entrar, destino: '+req.session.dest+', variavel id:'+id+', usuarios online: '+JSON.stringify(onlineUsers))
 })  
 
+app.post('/api/send', (req, res) => { 
+	
+})  
+ 
 app.post('/api/resp', (req, res) => {
-	
+	//console.log('destino: '+onlineUsers.indexOf(req.session.dest))
+	if(onlineUsers.indexOf(req.session.dest) == -1){//O destino está online? se não, tchauzinho! 
+		res.send({envio: 1}) 
+		//console.log('O destino: '+req.session.dest+'Esta offline!')
+	}else{
+		res.send({envio: 2}) 
+		//console.log('O destino: '+req.session.dest+' Esta online!')
+	}
 })   
-
+ 
 app.post('/api/bye', (req, res) => {
-
-	
+	var pos = onlineUsers.indexOf(req.session.name)
+	onlineUsers.splice(pos, pos)
+	console.log('"/api/bye" O úsuario '+req.session.name+' ficou offline! pessoas online no momento: '+onlineUsers)
+	res.redirect(303, '/')
 })
  
  app.post('/loading', (req, res) =>{
+		var pos = onlineUsers.indexOf(req.session.name)
 
+			if(onlineUsers.indexOf(req.session.name+1) == -1){//O destino está online? se não, tchauzinho!
+				res.send({m: false})
+			}else{
+				req.session.dest = onlineUsers.indexOf(req.session.name+1)
+				res.send({m: true})
+			}
  })
- 
-// página 404 personalizada
+  
+// página 404 personalizada 
 app.use((req, res) => {
  res.type('text/plain')
  res.status(404)
