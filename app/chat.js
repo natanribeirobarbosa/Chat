1b6b031d923e51d2f7633aa3e0cd1399e80dc3a4
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2022-10-23 01:32:31 -0300

    O back tem total controle sobre os úsuarios que estão online.

--- app/chat.js
+++ app/chat.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json())
 
 //configurando o cookie-parser
 const cookieParser = require('cookie-parser') 
-app.use(cookieParser(credentials.cookieSecret))
+app.use(cookieParser(credentials.cookieSecret)) 
 
 //configurando o express-session
 const expressSession = require('express-session')
@@ -45,19 +45,20 @@ app.use(expressSession({
 }))
  
 
-var id = -1
+var id = 0
 var mensagens = {}
 var onlineUsers = []
+var firstOnline = []
+var blacklist = []
 
 app.get('/', (req, res) => { 
 	res.render('index')
+	console.log('online users: '+onlineUsers+', Blacklist: '+blacklist)
 
 	
 })
   
 app.get('/chat', (req, res) => { 
-	
-	
 	if(req.session.name == null){//Se for a primeira vez do úsuario no site
 		id++ 
 		req.session.name = id
@@ -66,46 +67,94 @@ app.get('/chat', (req, res) => {
 		if(onlineUsers.indexOf(req.session.name) == -1){//o úsuario está online? se não, agora está.
 			onlineUsers.push(req.session.name)
 			}
+
 		
 		
 		var user = {id: req.session.name,
 							position: onlineUsers.indexOf(req.session.name)
 							} 
 		 
-		if(req.session.dest == undefined){ //Já tem um destino definido? caso não arranjamos um novo!
+	
 		 
 		if(user.position % 2 != 0){//A posição do úsuario no array é impar? se sim,  seu destino é uma posição atrás no array
 			var i = onlineUsers.indexOf(user.id)-1
 			console.log( onlineUsers.indexOf(user.id)-1)
 			user.dest = onlineUsers[i]
 			req.session.dest = user.dest
-		}else{//Então é ... Seu destino estará uma casa a frente...
-			var i = onlineUsers.indexOf(user.id)+1
-			if(onlineUsers[i] == undefined){//Já existe um destino? se não, terá de esperar
-				user.dest = -1
-			}else{
-				user.dest = onlineUsers[i]
-				req.session.dest = user.dest
-			} 
+		}else{	//Você é par,  Seu destino estará uma casa a frente...
+			req.session.dest = -1
 		}
-		}
-			
+		
 			if(onlineUsers.indexOf(user.dest) == -1){//o destino está online? se não, o user terá que esperar
 				res.render('chat', {message: 1})
-			}else{
+				console.log('Destino do user não existe...')
+			}else{ 
+				console.log('Destino existe...')
 				res.render('chat', {message: 2}) 
 			}
 			 //console.log(req.session.dest)user
 	console.log('"/" usuario '+req.session.name+' acabou de entrar, destino: '+req.session.dest+', variavel id:'+id+', usuarios online: '+JSON.stringify(onlineUsers))
 })  
 
-app.post('/api/send', (req, res) => { 
+//setInterval(blacklistfunc, 3000)
+blacklistfunc()
+ function blacklistfunc(){ 
+	console.log(blacklist)
+	 for(let index = 0; index < blacklist.length; index++){
+			var user = blacklist[index]
+			var pos = onlineUsers.indexOf(user)
+			
+			onlineUsers.splice(pos, 1)
+			blacklist.splice(index, 1)
+			
+}
+console.log('Varredura completa! Usuarios online: '+onlineUsers+', blacklist: '+blacklist)
+	for(let index2 = 0; index2 < onlineUsers.length; index2++){
+		
+		var user2 = onlineUsers[index2]
+		blacklist.push(user2)
+		
+	} 
+			console.log('Escrita completa! Usuarios online: '+onlineUsers+', blacklist: '+blacklist)
+			
+			setTimeout(blacklistfunc, 1000)
+}
+
+  
+
+app.post('/api/online', (req, res) => { 
+if( blacklist.indexOf(req.session.name) != -1){
+	var pos = blacklist.indexOf(req.session.name)
+blacklist.splice(pos, 1)}
+		
+
+	//	if(onlineUsers.indexOf(req.session.name) == -1){//Você está online?
+	//	res.send({envio: 1})
+		
+	//}else{
+		res.send({envio: 2})
+	//}
+		
+		res.end()
 	
+	
+	
+	//req.session.dest = null
+	
+})
+
+app.post('/api/send', (req, res) => { 
+	res.send()
+	res.end()
 })  
  
 app.post('/api/resp', (req, res) => {
 	//console.log('destino: '+onlineUsers.indexOf(req.session.dest))
+	//console.log(onlineUsers.indexOf(req.session.dest))
 	if(onlineUsers.indexOf(req.session.dest) == -1){//O destino está online? se não, tchauzinho! 
+		req.session.dest = null
+		var pos = onlineUsers.indexOf(req.session.name)
+		onlineUsers.splice(pos, 1)
 		res.send({envio: 1}) 
 		//console.log('O destino: '+req.session.dest+'Esta offline!')
 	}else{
@@ -114,22 +163,26 @@ app.post('/api/resp', (req, res) => {
 	}
 })   
  
+ /*
 app.post('/api/bye', (req, res) => {
 	var pos = onlineUsers.indexOf(req.session.name)
-	onlineUsers.splice(pos, pos)
-	console.log('"/api/bye" O úsuario '+req.session.name+' ficou offline! pessoas online no momento: '+onlineUsers)
-	res.redirect(303, '/')
-})
- 
+	onlineUsers.splice(pos, 1)
+	req.session.dest = null
+	console.log('"/api/bye" O úsuario '+pos+' ficou offline! pessoas online no momento: '+onlineUsers)
+	res.redirect(303, '/chat')
+}) */
+   
  app.post('/loading', (req, res) =>{
-		var pos = onlineUsers.indexOf(req.session.name)
+	 	var dest = onlineUsers.indexOf(req.session.name)+1
+				req.session.dest = onlineUsers[dest]
+	 
+	if(onlineUsers.indexOf(req.session.dest) != -1){
 
-			if(onlineUsers.indexOf(req.session.name+1) == -1){//O destino está online? se não, tchauzinho!
-				res.send({m: false})
-			}else{
-				req.session.dest = onlineUsers.indexOf(req.session.name+1)
-				res.send({m: true})
-			}
+		res.send({m: true})
+	}else{
+		res.send({m:false})
+	}
+	res.end()
  })
   
 // página 404 personalizada 
