ac69bf5d525772cd25a9ec6dbbe94e42a83de753
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2022-11-13 02:14:44 -0300

    Tudo certinho, porém houve uma tentativa de um teste que deve ser descartada.

--- app/chat.js
+++ app/chat.js
@@ -1,17 +1,11 @@
-/*
 
-variavel id = serve para nomear os úsuarios com um "id".
-array onlineUsers = serve para definir com quem cada pessoa irá conversar.
-
-*/
-
-
-//isso vai para o branch "vetor"
 const express = require('express')
 const app = express()
+const flash = require('./lib/middleware/flash')
 
 const port = process.env.PORT || 3000
 const { credentials } = require('./config')
+
 //definindo o renderizador padrão 
 const handleBars = require('express-handlebars');
 app.engine('handlebars', handleBars.engine({
@@ -43,7 +37,10 @@ app.use(expressSession({
 	secret: credentials.cookieSecret, 
 	maxAge: 30 * 24 * 60 * 60 * 1000
 }))
- 
+  
+//definindo o middleware de mensagens flash
+app.use(flash)
+
 
 var id = 0
 var mensagens = {}
@@ -54,141 +51,186 @@ var blacklist = []
 app.get('/', (req, res) => { 
 	res.render('index')
 	console.log('online users: '+onlineUsers+', Blacklist: '+blacklist+', mesagens: '+JSON.stringify(mensagens))
-
-	
 })
   
-app.get('/chat', (req, res) => { 
-	if(req.session.name == null){//Se for a primeira vez do úsuario no site
+app.get('/chat', async (req, res) => { 
+	if(req.session.name == null){//Recebe um novo id caso seja sua primeira vez no site
 		id++ 
 		req.session.name = id
+		
 		}
-		
-		if(onlineUsers.indexOf(req.session.name) == -1){//o úsuario está online? se não, agora está.
-			onlineUsers.push(req.session.name)
-			}else{
-				onlineUsers.splice(onlineUsers.indexOf(req.session.name), 1)
-				onlineUsers.push(req.session.name)
-			}
-
-		
-		
+		if(onlineUsers.indexOf(req.session.name)  != -1){
+				//onlineUsers.splice(onlineUsers.indexOf(req.session.name), 1)
+				req.session.flash = {
+					type: 'flashDanger',
+					message: '<h1>Erro!</h1>'
+				}
+				res.redirect('/')
+			
+		}else{
+				onlineUsers.push(req.session.name) 
+				
+				res.cookie('id', req.session.name)
 		var user = {id: req.session.name,
 							position: onlineUsers.indexOf(req.session.name)
 							} 
+							
 		 
 	mensagens[user.id] = []
 		 
 		if(user.position % 2 != 0){//A posição do úsuario no array é impar? se sim,  seu destino é uma posição atrás no array
-			var i = onlineUsers.indexOf(user.id)-1
-			console.log( onlineUsers.indexOf(user.id)-1)
+			var i =  user.position-1
 			user.dest = onlineUsers[i]
 			req.session.dest = user.dest
+			res.render('chat', {message: 1})
+			
 		}else{	//Você é par,  Seu destino estará uma casa a frente...
-			req.session.dest = -1
+			req.session.dest = user.position+1
+			res.cookie('dest', user.position+1)
+			res.render('chat', {message: 2}) 
 		}
-		
-			if(onlineUsers.indexOf(user.dest) == -1){//o destino está online? se não, o user terá que esperar
-				res.render('chat', {message: 1})
-				console.log('Destino do user não existe...')
-			}else{ 
-				console.log('Destino existe...')
-				res.render('chat', {message: 2}) 
-			}
-			 //console.log(req.session.dest)user
+	
 	console.log('"/" usuario '+req.session.name+' acabou de entrar, destino: '+req.session.dest+', variavel id:'+id+', usuarios online: '+JSON.stringify(onlineUsers))
-})  
+	
+		}})  
 
-//setInterval(blacklistfunc, 3000)
-blacklistfunc()
- function blacklistfunc(){ 
-	console.log(blacklist)
-	 for(let index = 0; index < blacklist.length; index++){
-			var user = blacklist[index]
-			var pos = onlineUsers.indexOf(user)
-			
-			onlineUsers.splice(pos, 1)
-			blacklist.splice(index, 1)
-			
-}
-console.log('Varredura completa! Usuarios online: '+onlineUsers+', blacklist: '+blacklist)
-	for(let index2 = 0; index2 < onlineUsers.length; index2++){
-		
-		var user2 = onlineUsers[index2]
-		blacklist.push(user2)
-		
-	} 
-			console.log('Escrita completa! Usuarios online: '+onlineUsers+', blacklist: '+blacklist)
-			
-			setTimeout(blacklistfunc, 1000)
-}
 
-  
 
-app.post('/api/online', (req, res) => { 
-if( blacklist.indexOf(req.session.name) != -1){
-	var pos = blacklist.indexOf(req.session.name)
-	blacklist.splice(pos, 1)}
-		
-	if(onlineUsers.indexOf(req.session.name) == -1){//Você está online?
-		res.send({envio: 1})
-		
-	}else{
-		res.send({envio: 2})
-	}
-		
+app.post('/api/chat2', (req, res) => { 
+	if(req.session.name == null){//envia erro caso o user não tenha vindo de outra conversa
+	console.log('requisição feita!')
+		res.send({erro: true})
 		res.end()
-	
-	
-	
-	//req.session.dest = null
-	
+		}else{
+		if(onlineUsers.indexOf(req.session.name)  != -1){
+			var pos = onlineUsers.indexOf(req.session.name)
+			onlineUsers.splice(pos, 1)
+			res.end()
+		}else{
+		
+				onlineUsers.push(req.session.name)
+				res.cookie('id', req.session.name)
+		var user = {id: req.session.name,
+							position: onlineUsers.indexOf(req.session.name)
+							}  
+							
+		 
+	mensagens[user.id] = []
+		 
+		if(user.position % 2 != 0){//A posição do úsuario no array é impar? se sim,  seu destino é uma posição atrás no array
+			var i =  user.position-1
+			user.dest = onlineUsers[i]
+			req.session.dest = user.dest
+			res.send({message: 1})
+			res.end()
+			console.log('chegou até aqui!1')
+			
+			
+		}else{	//Você é par,  Seu destino estará uma casa a frente...
+			req.session.dest = user.position+1
+			res.cookie('dest', user.position+1)
+			res.send({message: 2}) 
+			res.end()
+			console.log('chegou até aqui!2')
+		}
+	console.log('chegou até aqui!3')
+	console.log('"/" usuario '+req.session.name+' acabou de entrar, destino: '+req.session.dest+', variavel id:'+id+', usuarios online: '+JSON.stringify(onlineUsers))
+	 
+		}
+		}
+		})  
+
+app.get('/teste', (req, res) => {
+	res.render('choices')
 })
 
+
+ 
 app.post('/api/send', (req, res) => { 
 	mensagens[req.session.dest].push(req.body.texto)
 	console.log(req.body.texto)
-	res.send({chave: 'valor'})
-	
+	res.json({sended: req.body.texto})
 	res.end()
+	
+
 })  
  
 app.post('/api/resp', (req, res) => {
 	//console.log('destino: '+onlineUsers.indexOf(req.session.dest))
 	//console.log(onlineUsers.indexOf(req.session.dest))
+	
+	
+	
+	
+	
+	
+	
+	if(onlineUsers.indexOf(req.session.name) == -1){
+		res.send({meOffline: true})
+	}else{
+	
+	
 	if(onlineUsers.indexOf(req.session.dest) == -1){//O destino está online? se não, tchauzinho! 
 		req.session.dest = null
 		var pos = onlineUsers.indexOf(req.session.name)
 		onlineUsers.splice(pos, 1)
-		res.send({envio: 1}) 
+	
+		
+		res.send({offline: true})
+		res.end()
 		//console.log('O destino: '+req.session.dest+'Esta offline!')
 	}else{
-		if(mensagens[req.session.name].length != 0){
-			var msg = mensagens[req.session.name][0]
+	
+	if(mensagens[req.session.name].length == 0){
+		res.send({"mensagem": undefined}) 
+	}else{
+	
+		var msg = []
+		msg.push(mensagens[req.session.name][0])
+		if(msg.length != 0){
+			
 			mensagens[req.session.name].splice(mensagens[req.session.name][0], 1)
-			res.send({envio: 2, mensagem: msg}) 
+			res.send({"mensagem": msg}) 
 		}else{
-			res.send({envio: 2}) 
-		}
-		
-		//console.log('O destino: '+req.session.dest+' Esta online!')
-	}
-	res.end()
+			res.end()
+	}}
+	}}
+
 })   
  
    
  app.post('/loading', (req, res) =>{
-	 	var dest = onlineUsers.indexOf(req.session.name)+1
-				req.session.dest = onlineUsers[dest]
+	 if(onlineUsers.indexOf(req.session.name)  == -1){
+			res.send({m: 'erro'})
+	 }else{
+				
 	 
-	if(onlineUsers.indexOf(req.session.dest) != -1){
+	if(onlineUsers[req.session.dest] == undefined){
 
-		res.send({m: true})
+		res.send({m: false})
 	}else{
-		res.send({m:false})
-	}
-	res.end()
+		var dest = onlineUsers[req.session.dest]
+		res.cookie('dest', dest)
+		req.session.dest = dest
+		res.send({m:true})
+	 }}
+	res.end() 
  })
+  
+ app.post('/api/bye', (req, res) => {
+	var pos = onlineUsers.indexOf(req.session.name)
+	mensagens[req.session.name] = undefined
+	if(pos == -1){
+		res.end()
+	}else{
+	 onlineUsers.splice(pos, 1)
+	 
+	  console.log("o user: "+req.session.name+" saiu! úsuarios online agora: "+onlineUsers, pos)
+	res.end()
+	
+	}
+ })
+  
   
 // página 404 personalizada 
 app.use((req, res) => {
